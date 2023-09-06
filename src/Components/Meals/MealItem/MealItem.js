009b46d90cff2bf5535fb292ff695ea6f27e14c6
abhijivani3001 <abhijivani3001@gmail.com>
2023-09-06 11:33:13 +0530

    cart-context

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,8 +1,22 @@
-import classes from './MealItem.module.css'
+import { useContext } from 'react';
+import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
+import CartContext from '../../../store/cart-context';
 
-const MealItem=(props)=>{
-  const price=`$${props.price.toFixed(2)}`;
+const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
   return (
     <li className={classes.meal}>
       <div>
@@ -10,9 +24,11 @@ const MealItem=(props)=>{
         <div className={classes.description}>{props.description}</div>
         <h3 className={classes.price}>{price}</h3>
       </div>
-      <div><MealItemForm /></div>
+      <div>
+        <MealItemForm onAddToCart={addToCartHandler} />
+      </div>
     </li>
-  )
-}
+  );
+};
 
 export default MealItem;
